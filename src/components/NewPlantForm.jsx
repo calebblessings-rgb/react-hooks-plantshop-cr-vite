1dea103107079c3d5492306cb922ed1a0d254194
Caleb Baraka <caleb.blessings@student.moringaschool.com>
2026-08-12 14:03:50 +0300

    added sold out function

--- src/components/NewPlantForm.jsx
+++ src/components/NewPlantForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NewPlantForm({ addPlant }) {
-  const handleSumbit (event) => {
+  const handleSumbit = (event) => {
     event.PreventDefault();
     const newPlant = {
       name: event.target.name.value,
--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 function PlantCard({ plant }) {
+  const [SoldOut, setSoldOut] = useState(false);
+
+  const handleSumbit = () => {
+    setSoldOut(!SoldOut);
+  }
   return (
     <li className="card" data-testid="plant-item">
       <img src={plant.image} 
       alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {plant.InStock ? (
-        <button className="primary">In Stock</button>
-      ) : (
+      
+      {soldOut ? (
         <button>Out of Stock</button>
+        ) : (
+        <button
+          className="primary"
+          onClick={handleClick}
+        >
+          In Stock
+        </button>
       )}
     </li>
   );
 }
-
+ 
 export default PlantCard;
